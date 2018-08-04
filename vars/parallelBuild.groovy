37df7796133dfc860556447e3af7d7fb6d6c81b1
Ghost Yang <ghosttw88@hotmail.com>
2018-08-04 19:54:55 +0800

    remove stages

--- vars/parallelBuild.groovy
+++ vars/parallelBuild.groovy
@@ -7,7 +7,7 @@ def call(body) {
     echo "node"
     node {
         //parallel {
-            stages {
+            // stages {
                 stage 'Update' { 
                     echo "Update ${config.Name} ${config.Product} ${config.Is}"
                 }
@@ -17,7 +17,7 @@ def call(body) {
                 stage 'Deploy' { 
                     echo "Deploy ${config.Name} ${config.Product} ${config.Is}"
                 }
-            }
+            // }
         //}
     }
 }
